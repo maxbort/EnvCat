982fc09342fa8dff34844f8b84d204150f7974c3
ijin <2zun@naver.com>
2021-09-13 18:20:16 +0900

    initial

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+@Log4j2
+@RequiredArgsConstructor
+
+public class Controller {
+    @GetMapping("/index")
+    public String index(){
+        return "/index";
+    }
+}
